314adc7d6876eaa99d0ef8b5d1121c561ef6c897
Timo Celik <timo.celik@gmail.com>
2021-05-24 04:01:40 +0200

    Fix for smaller screens and add screen too small warning

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import { PageFlip } from "page-flip";
 import "normalize.css";
+import { PageFlip } from "page-flip";
 import "animate.css";
 import "./style.sass";
 
 const pageFlip = new PageFlip(document.querySelector("#booklet"), {
+  size: "stretch",
   width: 600,
   height: 600,
+  minWidth: 150,
+  maxWidth: 600,
+  minHeight: 150,
+  maxHeight: 600,
   showCover: true,
+  usePortrait: false,
 });
 pageFlip.loadFromHTML(document.querySelectorAll(".page"));
